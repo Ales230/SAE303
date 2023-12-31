0db7935dd4ff366c8bc6f9fbd502ea352e79f104
Ilyès <ilyesboubou.ib@gmail.com>
2023-12-31 15:52:27 +0100

    test css + avancée moyens pédagogiques

---
+++ bootstrap.js
@@ -0,0 +1,9 @@
+function showImage(src) {
+    var imageElement = document.getElementById('hover-image');
+    imageElement.src = src;
+    document.getElementById('image-container').style.display = 'block';
+}
+
+function hideImage() {
+    document.getElementById('image-container').style.display = 'none';
+}
